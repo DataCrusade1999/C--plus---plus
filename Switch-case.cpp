a2eb3d3a26365fc6ffe5e18d25aad4409ce8314c
DataCrusade1999 <ashutosh.pandeyhlr007@gmail.com>
2020-11-01 22:11:32 +0530

    Switch Case Updated

--- Switch-case.cpp
+++ Switch-case.cpp
@@ -1,6 +1,40 @@
 #include<iostream>
 using namespace std;
+string getdayname(int a)
+{
+    switch (a)
+    {
+    case 1:
+    return "Monday";
+    break;
+    case 2:
+    return "Tuesday";
+    break;
+    case 3:
+    return "Wednesday";
+    break;
+    case 4:
+    return "Thursday";
+    break;
+    case 5:
+    return "Friday";
+    break;
+    case 6:
+    return "Saturday";
+    break;
+    case 0:
+    return "Sunday";
+    break;
+    default:
+    return "Invalid Choice";
+    break;
+    }
+}
 int main()
 {
+    int a;
+    cout<<"Enter Your Day Number?"<<endl;
+    cin>>a;
+    cout<<getdayname(a);
     return 0;
 }
