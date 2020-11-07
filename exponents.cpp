d35a2bc9827995b2a6c913b759671aab2b4eb9a6
DataCrusade1999 <ashutosh.pandeyhlr007@gmail.com>
2020-11-07 16:50:45 +0530

    exponent

--- exponents.cpp
+++ exponents.cpp
@@ -1,7 +1,16 @@
 #include<iostream>
 using namespace std;
+int exp(int base,int power)
+{
+    int result=1;
+    for(int i=0;i<power;i++)
+    {
+        result=result*base;
+    }
+    return result;
+}
 int main()
 {
-    
+    cout<<exp(4,2);
     return 0;
 }
